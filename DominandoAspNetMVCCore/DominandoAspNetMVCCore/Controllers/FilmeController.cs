a87cf4d99d7602d9076c97e075988b7db64bd4a8
luis <luiscastrodev@gmail.com>
2021-01-08 23:38:16 -0300

    criacao formulario inicial

---
+++ DominandoAspNetMVCCore/DominandoAspNetMVCCore/Controllers/FilmeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DominandoAspNetMVCCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DominandoAspNetMVCCore.Controllers
+{
+    public class FilmeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar(Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View(filme);
+        }
+    }
+}
---
+++ DominandoAspNetMVCCore/DominandoAspNetMVCCore/Models/Filme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DominandoAspNetMVCCore.Models
+{
+    public class Filme
+    {
+
+        public int ID { get; set; }
+
+        [Required]
+        public string Titutlo { get; set; }
+
+        [Required]
+        public string Genero { get; set; }
+
+        [Required]
+        public DateTime DataLancamento { get; set; }
+
+        [Required]
+        public Decimal Valor { get; set; }
+    }
+}
