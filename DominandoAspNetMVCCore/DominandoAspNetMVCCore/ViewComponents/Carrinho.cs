115b0263775bb00ede0064d6e68ecad837111c8a
luis <luiscastrodev@gmail.com>
2021-01-08 23:11:29 -0300

    adicionando viewcomponent

---
+++ DominandoAspNetMVCCore/DominandoAspNetMVCCore/ViewComponents/Carrinho.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DominandoAspNetMVCCore.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")]
+    public class Carrinho : ViewComponent
+    {
+        public int Quantidade { get; set; }
+
+        public Carrinho()
+        {
+            this.Quantidade = 5;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(this.Quantidade);
+        }
+    }
+}
