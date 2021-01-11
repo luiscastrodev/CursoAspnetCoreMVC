bbe8b2f2535c9b8739ff0cdb53c6c3838598e49e
luis <luiscastrodev@gmail.com>
2021-01-10 22:36:26 -0300

    configuracao areas

---
+++ AppModelo/src/DevIO.UI.Site/Modulos/Produtos/Controllers/CadastroController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.UI.Site.Modulos.Produtos.Controllers
+{
+    [Area("Produtos")]
+    [Route("produtos")]
+    public class CadastroController : Controller
+    {
+        [Route("Lista")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AppModelo/src/DevIO.UI.Site/Startup.cs
+++ AppModelo/src/DevIO.UI.Site/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +17,15 @@ namespace DevIO.UI.Site
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            //configuracao da pasta area quando alterou o nome para modulos
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.AreaViewLocationFormats.Clear();
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+            });
 
             //adiciona mvc e seta versao 
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
@@ -43,7 +53,26 @@ namespace DevIO.UI.Site
                 routes.MapControllerRoute(
                      name: "default",
                      pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                //configurando uma area
+                /*routes.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  );*/
+
+                routes.MapAreaControllerRoute(
+                    name: "AreaProdutos",
+                    areaName: "Produtos",
+                    "{controller=Cadastro}/{action=Index}/{id?}"
+                    );
+                routes.MapAreaControllerRoute(
+                   name: "AreaVendas",
+                   areaName: "Pedidos",
+                   "{controller=Pedidos}/{action=Index}/{id?}"
+                   );
             });
+
+
         }
     }
 }
